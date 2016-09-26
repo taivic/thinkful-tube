7340efc2bc632ea2990ca1a0fbd320da9edb9a53
taivic <vicky.yen.tai@gmail.com>
2016-09-26 11:36:14 -0500

    updated all files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	  	event.preventDefault();
 	    var searchTerm = $('#searchYoutube').val();
 	    getRequest(searchTerm);
+	    //display button more results next page
+	    $('#moreResults').append("<p>" + "<button type='button' id='resultsButton'>" + "Show More Results" + "</button>" + "</p>");
  	});
 
 	function getRequest(searchTerm){
@@ -18,12 +20,21 @@ $(document).ready(function(){
 		});
 	}
 
+	/*//show more results
+	$(document).on("click", ".resultsButton", function() {
+		$('#searchResults').append()
+	})*/
+
 	function showResults(results){
 		var html = "";
 		$.each(results, function(index,value){
 	    	html += '<p>' + value.snippet.title + '</p>';
 	    	console.log(value.snippet.title);
+	    	//show thumbnails, make images clickable
+	    	html += '<img src= value.snippet.thumbnails.medium.url>';
+	    	//<a href="'www.youtube.com/embed/' + value.id.videoID">
 	  	});
 		$('#searchResults').html(html);
 	}
+
 });
